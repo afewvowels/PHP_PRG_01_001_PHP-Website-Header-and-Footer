f2bd607d732cdb59659f41a8cc90e857d1bd6ff8
Keith Smith <32456218+afewvowels@users.noreply.github.com>
2018-09-18 17:24:30 -0400

    updated

--- includes/inc_menu.php
+++ includes/inc_menu.php
@@ -1,14 +1,14 @@
 <nav>
   <div id='menu'>
     <div id='menu_icon'>
-      <img id='icon' class='menu_icon_dark'>
+      <img id='icon'>
       <script src='./js/menu.js'></script>
     </div>
     <ul id='menu_items'>
-      <li class='menu_item'><div><img src='./img/icons/info.svg' class='icon'><p>About</p></div></li>
-      <li class='menu_item'><div><img src='./img/icons/info.svg' class='icon'><p>About</p></div></li>
-      <li class='menu_item'><div><img src='./img/icons/info.svg' class='icon'><p>About</p></div></li>
-      <li class='menu_item'><div><img src='./img/icons/info.svg' class='icon'><p>About</p></div></li>
+      <li class='menu_item'><img class='menu_icon'><p class='menu_item_title'>About</p></li>
+      <li class='menu_item'><img class='menu_icon'><p class='menu_item_title'>About</p></li>
+      <li class='menu_item'><img class='menu_icon'><p class='menu_item_title'>About</p></li>
+      <li class='menu_item'><img class='menu_icon'><p class='menu_item_title'>About</p></li>
     </ul>
   </div>
 </nav>
--- js/menu.js
+++ js/menu.js
@@ -1,8 +1,5 @@
-var elem = document.getElementById('icon');
+var icon = document.getElementById('icon');
 var menu = document.getElementById('menu');
-var menuicon = document.getElementById('menu_icon');
-elem.onclick = function() {
-  elem.classList.toggle('menu_icon_light');
-  menu.classList.toggle('menu');
-  menuicon.classList.toggle('menu_icon_move');
+icon.onclick = function() {
+  menu.classList.toggle('menu_move');
 };
